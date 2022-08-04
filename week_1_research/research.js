dec168b65bd4057c08fa0885b8301f0d3e8e127b
Shannon Beebe <beebegun2.0@gmail.com>
2022-08-04 13:21:41 -0500

    added 2nd question

--- week_1_research/research.js
+++ week_1_research/research.js
@@ -8,5 +8,5 @@ console.log("What is GIT?")
 
 console.log("GIT is a free, open source version control system that can handle everything from small to larg projects with speed and efficiency. It tracks changes in any set of files, and coordinates work among programmers developing source code. This makes it useful because, the goal is speed, data integrity, and support for distrubution, via workflows. The GIT workflow encourages developers and DevOps teams to leaverage GIT effectively and consistently. A normal workflow is to develop and check in on a branch, merge the branch back to the master. You create a local repository that consists of three trees, maintained by GIT, which the first one is the Working Directory that holds the actual file.")
 
-
+console.log("What was my favorite thing to learn this week?")
 console.log("My favorite thing I learned this week would be how to follow the Git workflow. I struggled with this in the beginning, but once I figured it out with the help of the Office Hours, and Mentor I was able to complete the process.")
