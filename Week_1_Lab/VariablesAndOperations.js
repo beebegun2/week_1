8ca3e01a4404c8ea42dbb97f49bc9f46b1729718
Shannon Beebe <beebegun2.0@gmail.com>
2022-08-04 12:30:12 -0500

    week 1 lab

---
+++ Week_1_Lab/VariablesAndOperations.js
@@ -0,0 +1,40 @@
+//create a variable to hold the quantity of available plane seats left on a flight
+var availablePlaneSeats = 5;
+		
+//create a variable to hold the cost of groceries at checkout
+var costOfGroceries = 15.50;
+
+//create a variable to hold a person's middle initial
+var middleInitial = "L";
+
+//create a variable to hold true if it's hot outside and false if it's cold outside
+var itsHotOutside = true;
+
+//create a variable to hold a customer's first name
+var customerFirstName = "Shannon";
+
+//create a variable to hold a street address
+var streetAddress = '568 Programmer Way';
+
+//print all variables to the console
+
+
+
+
+//a customer booked 2 plane seats, remove 2 seats from the available seats variable
+
+
+//impulse candy bar purchase, add 2.15 to the grocery total
+//costOfGroceries = costOfGroceries + 2.15;
+
+
+//birth certificate was printed incorrectly, change the middle initial to something else
+
+
+//the season has changed, update the hot outside variable to be opposite of what it was
+
+
+//create a new variable called full name using the customer's first name, the middle initial, and a last name of your choice
+
+
+//print a line to the console that introduces the customer and says they live at the address variable
