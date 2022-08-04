f1fa2d356f844ef406a6de7195f41f2737f56be7
Shannon Beebe <beebegun2.0@gmail.com>
2022-08-04 12:50:50 -0500

    added console log information

--- Week_1_Lab/VariablesAndOperations.js
+++ Week_1_Lab/VariablesAndOperations.js
@@ -5,7 +5,7 @@ var availablePlaneSeats = 5;
 var costOfGroceries = 15.50;
 
 //create a variable to hold a person's middle initial
-var middleInitial = "L";
+var middleInitial = "G";
 
 //create a variable to hold true if it's hot outside and false if it's cold outside
 var itsHotOutside = true;
@@ -16,25 +16,38 @@ var customerFirstName = "Shannon";
 //create a variable to hold a street address
 var streetAddress = '568 Programmer Way';
 
-//print all variables to the console
-
-
+//print all variables to the console (console.log)
+console.log(availablePlaneSeats)
+console.log(costOfGroceries)
+console.log(middleInitial)
+console.log(itsHotOutside)
+console.log(customerFirstName)
+console.log(streetAddress)
 
 
 //a customer booked 2 plane seats, remove 2 seats from the available seats variable
+availablePlaneSeats = availablePlaneSeats - 2;
+//always use console.log() to "print results to browser"
 
+console.log(availablePlaneSeats)
 
 //impulse candy bar purchase, add 2.15 to the grocery total
 //costOfGroceries = costOfGroceries + 2.15;
+costOfGroceries = costOfGroceries + 2.15;
 
+console.log(costOfGroceries)
 
 //birth certificate was printed incorrectly, change the middle initial to something else
-
+middleInitial = "L";
+console.log(middleInitial)
 
 //the season has changed, update the hot outside variable to be opposite of what it was
-
+itsHotOutside = !itsHotOutside;
+console.log(itsHotOutside)
 
 //create a new variable called full name using the customer's first name, the middle initial, and a last name of your choice
-
+var fullName = customerFirstName +" " + middleInitial + " Beebe";
+console.log(fullName)
 
 //print a line to the console that introduces the customer and says they live at the address variable
+console.log("Hi, my name is " + customerFirstName + " and I live at " + streetAddress + ".");
